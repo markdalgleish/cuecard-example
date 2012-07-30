eb790413cae0cb8365a8ac3b24bc3bd23d71c129
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-30 13:35:32 +1000

    Use new HTML, CSS and title config options

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var cuecard = require('cuecard');
 
+cuecard.configure({
+	title: 'Getting Closure',
+	html: __dirname + '/slides.html',
+	css: __dirname + '/style.css'
+});
+
 cuecard.server.listen(3000);
