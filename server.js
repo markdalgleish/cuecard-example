e85db8f5cb4ee4eed2fa1d6f42eee2665cb5d556
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-30 14:52:18 +1000

    Add Jade slides

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ cuecard.create({
 	port: 3000,
 	remoteUrl: '/my-remote-url',
 	title: 'Getting Closure',
-	html: __dirname + '/slides.html',
+	html: __dirname + '/slides.jade',
 	css: __dirname + '/slides.css'
 });
