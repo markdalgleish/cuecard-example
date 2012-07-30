50f47fddc1ae013506ae440699cee84235704b52
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-07-30 13:43:25 +1000

    Fix filename

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var cuecard = require('cuecard');
 cuecard.configure({
 	title: 'Getting Closure',
 	html: __dirname + '/slides.html',
-	css: __dirname + '/style.css'
+	css: __dirname + '/slides.css'
 });
 
 cuecard.server.listen(3000);
